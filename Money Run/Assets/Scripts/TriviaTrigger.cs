b6b5daadade5bafaf8b63031794940baecb318b7
kndede <96321932+kndede@users.noreply.github.com>
2022-02-01 17:10:13 +0200

    Trivia on works

---
+++ Money Run/Assets/Scripts/TriviaTrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class TriviaTrigger : MonoBehaviour
+{
+    public TextMeshProUGUI question;
+    public GameObject leftPanel;
+    public GameObject rightPanel;
+
+
+    private Collider myCol;
+    private void Awake()
+    {
+        myCol=gameObject.GetComponent<Collider>();
+    }
+    int triggerCount = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggerCount==0)
+        {
+            question.gameObject.SetActive(true);
+            leftPanel.SetActive(true);
+            rightPanel.SetActive(true);
+
+            Destroy(this.gameObject);
+           
+        }
+        
+            
+        
+    }
+}
