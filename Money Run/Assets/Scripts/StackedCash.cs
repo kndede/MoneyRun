f58cabedc0ef92ea99f2417653187515b5e9f01a
kndede <96321932+kndede@users.noreply.github.com>
2022-02-12 19:47:37 +0200

    money prefabs attached

--- Money Run/Assets/Scripts/StackedCash.cs
+++ Money Run/Assets/Scripts/StackedCash.cs
@@ -9,7 +9,9 @@ public class StackedCash : MonoBehaviour
     MeshRenderer myMesh;
     public bool isMeshActive=true;
     public int myIndex;
+
     public GameObject banknote;
+    public ParticleSystem dollarBlast;
 
     public float secondsToWait= 1f;
     public float jumpPower;
@@ -52,16 +54,21 @@ public class StackedCash : MonoBehaviour
     {
 
         DisableMeshRenderer();
+        dollarBlast.Play();
         float x = upperLeftCorner.x;
         float maxz = upperLeftCorner.y;
         float minz = downRightCorner.y;
         float rndZ = Random.Range(minz, maxz);
         float rndX = Random.Range(x, -x);
 
+
         GameObject newObject = Instantiate(banknote, transform.position, Quaternion.identity);
 
-        newObject.transform.DOJump(new Vector3(rndX, 0.8f, rndZ), jumpPower, 1, jumpDuration).OnComplete(() => newObject.tag="Cash");
+        newObject.transform.DOScale(new Vector3(2, 2, 2), jumpDuration);
+        newObject.transform.DORotate(new Vector3(0, 90f, 0), jumpDuration);
 
+        newObject.transform.DOJump(new Vector3(rndX, 0.8f, rndZ), jumpPower, 1, jumpDuration).OnComplete(() => newObject.tag="Cash");
+        
     }
 
    
